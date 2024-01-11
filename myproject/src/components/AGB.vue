91420e19cf738c1ed13710e911a8dc32e5b8e5a6
aaghdas <149900177+aaghdas@users.noreply.github.com>
2024-01-11 18:30:14 +0100

    AGB component

--- myproject/src/components/AGB.vue
+++ myproject/src/components/AGB.vue
@@ -1,8 +1,11 @@
 <template  >
 
     <div class="datenschutz">
+        <!-- Überschrift für die AGB -->
         <h1 class="datenschutz-title">   Allgemeine Geschäftsbedingungen(AGB)</h1><br/>
-        <p> {{ string }}</p>
+
+        <!-- Absatz, der den Text für die AGB enthält, der aus der Datenobjekt "string" stammt -->
+        <p> {{ string }}</p>  <!-- Variable Interpolation in Vue js-->
         
     </div>
     
@@ -12,10 +15,10 @@
     
      export default {
     
-    name:'AGB',
-    data(){
+    name:'AGB',  // Name der Komponente registrieren
+    data(){ 
             return{
-        string:
+        string:   // Zeichenkette, die den Textinhalt für die AGB enthält 
         "Durch die Nutzung unserer Dienste erklären Sie sich mit den folgenden Allgemeinen Geschäftsbedingungen einverstanden: Unsere Dienste dürfen ausschließlich in Übereinstimmung mit den geltenden Gesetzen und Vorschriften genutzt werden. Jegliche Verstöße gegen diese Bedingungen können zur Kündigung Ihres Kontos führen. Wir übernehmen keine Verantwortung für Verluste oder Schäden, die durch die Nutzung unserer Dienste entstehen. Die Nutzung unserer Dienste geschieht auf eigenes Risiko. "
             
     }
